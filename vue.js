12145d0905e3d19eb518d18fbde0b7562cc92486
hanchayi <hanchayi@163.com>
2023-08-04 17:04:50 +0800

    feat: create element

--- vue.js
+++ vue.js
@@ -1,31 +1,11 @@
-function h(tag, props, children) {
+import createElement from './createElement.js'
+
+export function h(tag, props, children) {
   return {
     tag,
     props,
     children
   }
-}
-
-function createElement(vnode) {
-  const element = document.createElement(vnode.tag)
-  Object.keys(vnode.props).forEach(prop => {
-    element.setAttribute(prop, vnode.props[prop])
-  })
-
-  if (vnode.children) {
-    if (Array.isArray(vnode.children)) {
-      vnode.children.forEach(child => {
-        element.appendChild(createElement(child))
-      })
-    } else if (typeof vnode.children === 'string') {
-      element.innerHTML = vnode.children
-    } else {
-      throw new Error('invalid vnode')
-    }
-  }
-
-  vnode.el = element
-  return element
 }
 
 function mount(vnode, container) {
@@ -36,10 +16,11 @@ function mount(vnode, container) {
 }
 
 export function createApp(App) {
+  const app = App.render(h)
   return {
     mount: (container) => {
-      const app = App.render(h)
       mount(app, container)
+      return app
     },
   }
 }
